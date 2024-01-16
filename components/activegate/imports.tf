a51a0eb2d6403cff0d7d15300858a8f69313b59a
Louise Huyton <louise.huyton@justice.gov.uk>
2024-01-16 11:46:48 +0000

    import prod private vmss resource

---
+++ components/activegate/imports.tf
@@ -0,0 +1,4 @@
+import {
+  to = azurerm_linux_virtual_machine_scale_set.main["dynatrace-activegate-private"].id
+  id = "/subscriptions/2b1afc19-5ca9-4796-a56f-574a58670244/resourceGroups/dynatrace-activegate-prod/providers/Microsoft.Compute/virtualMachineScaleSets/dynatrace-activegate-private-prod-vmss"
+}
