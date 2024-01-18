63571bd68ffe946bddbd9ef3f96ea7fa8e947566
Louise Huyton <louise.huyton@justice.gov.uk>
2024-01-18 10:56:09 +0000

    Add to moved.tf

--- components/activegate/moved.tf
+++ components/activegate/moved.tf
@@ -1,14 +1,49 @@
 moved {
-  from = module.vnet.azurerm_route_table.route_table
-  to   = module.networking.route_tables.rt
+  from = module.nsg.azurerm_network_security_group.network_security_group
+  to   = module.networking.azurerm_network_security_group.this["nsg"]
+}
+
+# moved {
+#   from =
+#   to   = module.networking.azurerm_route.this["rt-default"]
+# }
+
+# moved {
+#   from =
+#   to   = module.networking.azurerm_route_table.this["rt"]
+# }
+
+moved {
+  from = module.nsg.azurerm_subnet_network_security_group_association.subnet_association["dynatrace-activegate-subnet-0-nonprod"]
+  to   = module.networking.azurerm_subnet.this["dynatrace-activegate-subnet0"]
 }
 
 moved {
-  from = module.vnet.azurerm_subnet_route_table_association.subnet_00
-  to   = "module.networking.route_tables.rt.subnets.${var.name}-subnet0"
+  from = module.nsg.azurerm_subnet_network_security_group_association.subnet_association["dynatrace-activegate-subnet-1-nonprod"]
+  to   = module.networking.azurerm_subnet.this["dynatrace-activegate-subnet1"]
 }
 
+# moved {
+#   from =
+#   to   = module.networking.azurerm_subnet_network_security_group_association.this["nsg-dynatrace-activegate-subnet0"]
+# }
+
+# moved {
+#   from =
+#   to   = module.networking.azurerm_subnet_network_security_group_association.this["nsg-dynatrace-activegate-subnet1"]
+# }
+
+# moved {
+#   from =
+#   to   = module.networking.azurerm_subnet_route_table_association.this["rt-dynatrace-activegate-subnet0"]
+# }
+
+# moved {
+#   from =
+#   to   = module.networking.azurerm_subnet_route_table_association.this["rt-dynatrace-activegate-subnet1"]
+# }
+
 moved {
-  from = module.vnet.azurerm_subnet_route_table_association.subnet_01
-  to   = "module.networking.route_tables.rt.subnets.${var.name}-subnet1"
+  from = module.vnet.azurerm_virtual_network.vnet
+  to   = module.networking.azurerm_virtual_network.this["dynatrace-activegate"]
 }
