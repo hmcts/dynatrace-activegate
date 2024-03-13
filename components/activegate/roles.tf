47e32100fc1daaf0e1857245f0b373b350440739
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2024-03-13 14:26:09 +0000

    update role defs

--- components/activegate/roles.tf
+++ components/activegate/roles.tf
@@ -1,12 +1,3 @@
-# data "azuread_group" "admin_access" {
-#   display_name     = var.env == "prod" ? "DTS Production Dynatrace ActiveGate Access for Administrators" : "DTS Non-Production Dynatrace ActiveGate Access for Administrators"
-#   security_enabled = true
-# }
-# data "azuread_group" "user_access" {
-#   display_name     = var.env == "prod" ? "DTS Production Dynatrace ActiveGate Access for Users" : "DTS Non-Production Dynatrace ActiveGate Access for Users"
-#   security_enabled = true
-# }
-
 data "azurerm_role_definition" "virtual_machine_admin" {
   name = "Virtual Machine Administrator Login"
 }
@@ -15,14 +6,16 @@ data "azurerm_role_definition" "virtual_machine_user" {
   name = "Virtual Machine User Login"
 }
 
+data "azurerm_client_config" "current" {}
+
 resource "azurerm_role_assignment" "virtual_machine_admin" {
   scope              = azurerm_resource_group.rg.id
-  role_definition_id = data.azurerm_role_definition.virtual_machine_admin.role_definition_id
+  role_definition_id = "/subscriptions/${data.azurerm_client_config.current.subscription_id}/${data.azurerm_role_definition.virtual_machine_admin.role_definition_id}"
   principal_id       = local.admin_group
 }
 
 resource "azurerm_role_assignment" "virtual_machine_user" {
   scope              = azurerm_resource_group.rg.id
-  role_definition_id = data.azurerm_role_definition.virtual_machine_user.role_definition_id
+  role_definition_id = "/subscriptions/${data.azurerm_client_config.current.subscription_id}/${data.azurerm_role_definition.virtual_machine_user.role_definition_id}"
   principal_id       = local.user_group
 }
