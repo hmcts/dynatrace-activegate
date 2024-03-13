33e32297ec475bde4bf3d8918a4f7b20fc7b2b26
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2024-03-13 14:40:59 +0000

    remove slash

--- components/activegate/roles.tf
+++ components/activegate/roles.tf
@@ -10,12 +10,12 @@ data "azurerm_client_config" "current" {}
 
 resource "azurerm_role_assignment" "virtual_machine_admin" {
   scope              = azurerm_resource_group.rg.id
-  role_definition_id = "/subscriptions/${data.azurerm_client_config.current.subscription_id}/${data.azurerm_role_definition.virtual_machine_admin.role_definition_id}"
+  role_definition_id = "/subscriptions/${data.azurerm_client_config.current.subscription_id}${data.azurerm_role_definition.virtual_machine_admin.role_definition_id}"
   principal_id       = local.admin_group
 }
 
 resource "azurerm_role_assignment" "virtual_machine_user" {
   scope              = azurerm_resource_group.rg.id
-  role_definition_id = "/subscriptions/${data.azurerm_client_config.current.subscription_id}/${data.azurerm_role_definition.virtual_machine_user.role_definition_id}"
+  role_definition_id = "/subscriptions/${data.azurerm_client_config.current.subscription_id}${data.azurerm_role_definition.virtual_machine_user.role_definition_id}"
   principal_id       = local.user_group
 }
