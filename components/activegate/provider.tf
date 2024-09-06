c08ed8004365298259364cdcaeb36f0222588ad8
Louise Huyton <louise.huyton@justice.gov.uk>
2024-09-06 13:34:48 +0100

    add provider for storage account

--- components/activegate/provider.tf
+++ components/activegate/provider.tf
@@ -67,3 +67,9 @@ provider "azurerm" {
   features {}
   subscription_id = var.cnp_vault_sub
 }
+
+provider "azurerm" {
+  features {}
+  subscription_id = "1baf5470-1c3e-40d3-a6f7-74bfbce4b348"
+  alias           = "DTS-CFTPTL-INTSVC"
+}
