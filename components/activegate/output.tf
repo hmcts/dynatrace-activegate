8762feaae78e1382dde52b6eede812b234947c08
Louise Huyton <louise.huyton@justice.gov.uk>
2024-09-09 13:22:45 +0100

    change output.tf

--- components/activegate/output.tf
+++ components/activegate/output.tf
@@ -1,9 +1,3 @@
-data "null_data_source" "vm_outputs" {
-  inputs = {
-    xdr_tags = join(",", module.vm-bootstrap.*.XDR_TAGS)
-  }
-}
-
-output "vm_result" {
-  value = data.null_data_source.vm_outputs.*.inputs
+output "XDR_TAGS" {
+  value = { for k, v in module.vm-bootstrap : k => v.XDR_TAGS }
 }
