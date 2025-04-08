818cf328306b3523648174605a310eb66674619c
Dawid Strozak <dawid.strozak@hmcts.net>
2025-04-08 14:49:24 +0100

    Add another provider registration skip

--- components/activegate/provider.tf
+++ components/activegate/provider.tf
@@ -78,6 +78,7 @@ provider "azurerm" {
 
 provider "azurerm" {
   features {}
-  subscription_id = "1baf5470-1c3e-40d3-a6f7-74bfbce4b348"
-  alias           = "DTS-CFTPTL-INTSVC"
+  subscription_id            = "1baf5470-1c3e-40d3-a6f7-74bfbce4b348"
+  alias                      = "DTS-CFTPTL-INTSVC"
+  skip_provider_registration = "true"
 }
